7688d25ab33dcebdd26e5268f95ef8bf8d2b1d24
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-13 19:10:35 +0300

    Добавил в str_func.py функцию преобразования первых букв в заглавные. Плюс добавил ко всем функциям докстринги

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def get_capital_letters(user_line: str) -> str:
+    """Функция преобразования всех букв в заглавные"""
     return user_line.upper()
 
+def get_title_letters(user_line: str) -> str:
+    """Функция преобразования первых букв в заглавные"""
+    return user_line.title()
+
+print(get_title_letters('asf dsf fdf'))
