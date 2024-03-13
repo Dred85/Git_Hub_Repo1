c5eae28eb414e00163691554aa5b3de961b79457
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-13 19:15:36 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,4 @@
 def get_capital_letters(user_line: str) -> str:
-    """Функция преобразования всех букв в заглавные"""
+    """Функция преобразования букв в заглавные"""
     return user_line.upper()
 
