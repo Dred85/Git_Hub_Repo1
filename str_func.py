89bc5cdde98ab424dbd65c58de41bde82f2b0cc9
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-13 19:18:07 +0300

    В файле str_func.py Дописал докстринги к функциям

--- str_func.py
+++ str_func.py
@@ -1,10 +1,10 @@
 def get_capital_letters(user_line: str) -> str:
-    """Функция преобразования всех букв в заглавные"""
+    """Функция для преобразования всех букв в заглавные"""
     return user_line.upper()
 
 
 def get_title_letters(user_line: str) -> str:
-    """Функция преобразования первых букв в заглавные"""
+    """Функция для преобразования первых букв в заглавные """
     return user_line.title()
 
 print(get_title_letters('asf dsf fdf'))
