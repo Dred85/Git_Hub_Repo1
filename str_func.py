f5f88792da43d1646619743c0820a624ca7a066a
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-13 20:11:33 +0300

    В файле str_func.py добавил strip() к первой функции

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def get_capital_letters(user_line: str) -> str:
     """Функция преобразования всех букв в заглавные"""
-    return user_line.upper()
+    return user_line.upper().strip()
 
 
 def get_title_letters(user_line: str) -> str:
