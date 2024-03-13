964851d48e75f117ab32821d9eaa569921c435ab
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-13 19:32:43 +0300

    В файле str_func.py Решил конфликт, отклонил правки другого программиста с гитхаб

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 def get_capital_letters(user_line: str) -> str:
-    """Функция преобразования букв в заглавные"""
+    """Функция преобразования всех букв в заглавные"""
     return user_line.upper()
 
+
+def get_title_letters(user_line: str) -> str:
+    """Функция преобразования первых букв в заглавные"""
+    return user_line.title()
+
+print(get_title_letters('asf dsf fdf'))
